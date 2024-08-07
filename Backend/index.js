88bacbb947c4ea70ccdde8d8bc3d3de4452c955e
Mohd Mustajab <mohdmustajab002@gmail.com>
2024-08-07 22:43:01 +0530

    ./

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,8 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+console.log(process.env.CLOUDINARY_CLOUD_NAME, process.env.CLOUDINARY_API_KEY, process.env.CLOUDINARY_API_SECRET);
+
 
 ffmpeg.setFfmpegPath(require('ffmpeg-static'));
 
