9f5a860daf07517a5ab5b615e4f035b012bdccc4
Mohd Mustajab <mohdmustajab002@gmail.com>
2024-08-12 23:52:05 +0530

    add play button

--- Frontend/components/Video_list.jsx
+++ Frontend/components/Video_list.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
-const Video_list = ({ videos, onVideoSelect, onDelete ,isdeleting}) => {
+const Video_list = ({ videos, onVideoSelect, onDelete, isdeleting }) => {
   return (
     <div>
       <h2>Uploaded Videos</h2>
       <ul>
         {videos.map((video) => (
           <li key={video._id}>
-            <span onClick={() => onVideoSelect(video.filename)}>{video.filename}</span>
-            <button onClick={() => onDelete(video._id)}   disabled={isdeleting[video.id]}>  {isdeleting[video.id] ? 'Deleting...' : 'Delete'}</button>
+            <span>{video.filename}</span>
+            <button onClick={() => onVideoSelect(video.filename)}>Play</button>
+            <button 
+              onClick={() => onDelete(video._id)} 
+              disabled={isdeleting[video._id]}
+            >
+              {isdeleting[video._id] ? 'Deleting...' : 'Delete'}
+            </button>
           </li>
         ))}
       </ul>
